713c50b0e98802a6a2cbb8664a673cdf14ea1484
Lucas Dickmann <131892016+ldickmann@users.noreply.github.com>
2024-09-17 03:07:45 -0300

    correção das pastas e inclusão dodiretório 'javascript-DOM'

---
+++ FrontEndG7-ONE/Javascript-DOM/exercicios/modulo-Iniciando/main.js
@@ -0,0 +1,34 @@
+// Exercicio 01
+
+document.querySelector('html');
+document.querySelector('#timer');
+document.querySelector('.app__image');
+document.querySelector('.app__title');
+
+
+// Exercicio 02
+
+const html = document.querySelector('html');
+const timer = document.querySelector('#timer');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__titulo');
+
+console.log(html);
+console.log(timer);
+console.log(banner);
+console.log(titulo);
+
+
+// Exercicio 03
+
+const btnIniciar = document.querySelector('.app__card-primary-button');
+const focoBtn = document.querySelector('.app__card-button--foco');
+const curtoBtn = document.querySelector('.app__card-button--curto');
+const longoBtn = document.querySelector('.app__card-button--longo');
+
+
+// Exercicio 04
+
+const timeFoco = 1500;
+const timeCurto = 300;
+const timeLongo = 900;
